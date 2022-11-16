43671acc445c1140940ed44c7bde999434abdd9b
MWoodlens <mesiuswoodlens71@gmail.com>
2022-11-15 23:32:06 -0500

    second commit

--- ex10.js
+++ ex10.js
@@ -1,8 +1,5 @@
-let token=window.localStorage.getItem('blast');
-if(token!==null){
+let token="web-insecure;34829sjdfnsj32984madsdkj";
     console.log("token avan : "+token);
     token=token.split(';').pop();
     console.log("token apre : "+token);
-}else{
-    console.log('pa gen pyes token ki anrejistre nan navigate a ak kle sa');
-}
+
